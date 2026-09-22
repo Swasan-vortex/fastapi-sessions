aa44fa2203d8b7fe971149d151e090641039e31a
Aditi <upadhyayaditi2005@gmail.com>
2026-09-22 20:33:52 +0530

    Added about parameters

--- project1.py
+++ project1.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import pstats
+
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -14,7 +16,7 @@ def load_data():
 def hello():
     return {'message': 'Patient Management System API'}
 
-@app.get("/about")
+@app.get("about")
 def about():
     return {'message': "Fully functional API for manageing your patients record"}
 
@@ -25,11 +27,30 @@ def view():
     return data
 
 @app.get("/patients/{patient_id}")
-def view_patient(patient_id: str):
-    # load all the patients
+def view_patient(patient_id: str = Path(..., description='ID of the patient to view in the DB', example='P001')): 
+    #LOAD ALL THE PATIENTS
     data = load_data()
 
     if patient_id in data:
         return data[patient_id]
-    return {'error': 'Patient not found'}
+    raise HTTPException(status_code=404, detail='Patient id not found')
+
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description= 'Sort on the basis of height, weight, and bmi'), order: str = Query('desc', description= 'sort in  desc order' )):
+
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException (status_code=400, details='Invalid field select from {valid_fields}')
+
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, details= 'Invalid order select between asc and desc')
+
+    data = load_data()
+
+    sort_order = 'True' if order == 'desc' else 'False'
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data 
 
